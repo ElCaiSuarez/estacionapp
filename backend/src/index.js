5a8712313696d7f00daca2a1ad00404c821f28de
Juan Carlos Suarez <caisuarez@gmail.com>
2022-11-01 22:17:08 -0300

    agregamos constantes en backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,10 +7,36 @@ import cors from 'cors'
 const app = express() 
 app.use(cors());
 app.use(bodyParser.json());
-
+//Entidades
+const users = [{
+    id: 0, name: "usuario1@sistema.com", password: "123456",
+    id: 1, name: "usuario2@sistema.com", password: "123456",
+    id: 2, name: "usuario3@sistema.com", password: "123456",
+    id: 3, name: "usuario4@sistema.com", password: "123456"
+}]
+const vehicles = [{
+    id: 0, name: "Vehiculo1", userId: 1,
+    id: 1, name: "Vehiculo2", userId: 1,
+    id: 2, name: "Vehiculo3", userId: 2,
+    id: 3, name: "Vehiculo4", userId: 3
+}]
+const locations = [{
+    id: 0, name: "Barrio1",
+    id: 1, name: "Barrio2",
+    id: 2, name: "Barrio3",
+    id: 3, name: "Barrio4"
+}]
+const parkings = [{
+    id: 0, name: "Estacionamiento1", locationId: 0, userId: 1,
+    id: 1, name: "Estacionamiento2", locationId: 0, userId: 2,
+    id: 2, name: "Estacionamiento3", locationId: 1, userId: 3,
+    id: 3, name: "Estacionamiento4", locationId: 3, userId: 3
+}]
+//POST LOGIN
 app.post('/api/login', (req,res) => {
+    const usuarioAux = users.filter(usuario => usuario.name = req.body.email);
     console.log(req.body);
-    if(req.body && req.body.email == "usuario@sistema.com" && req.body.password == "123456"){
+    if(req.body && usuarioAux.length == 1 && req.body.password == usuarioAux[0].password){
         res.sendStatus(200);
     }else{
         res.sendStatus(401);
